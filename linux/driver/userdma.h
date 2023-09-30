1ae8ecb29cb31e26c658d11f605b6e3f250fdcc5
natanbc <natanbc@users.noreply.github.com>
2023-09-30 05:03:29 -0300

    userdma: add dma_buf physical address retrieval function, for single SG entry buffers only

--- linux/driver/userdma.h
+++ linux/driver/userdma.h
@@ -8,7 +8,13 @@ struct userdma_buf_creation_data {
     /* out */ void* phys_addr;
 };
 
-#define USERDMA_IOCTL_PRINT _IOR('u', 1, int)
-#define USERDMA_IOCTL_ALLOC _IOWR('u', 2, struct userdma_buf_creation_data)
+struct userdma_phys_addr_data {
+    /* in */ int fd;
+    /* out */ void* phys_addr;
+};
+
+#define USERDMA_IOCTL_PRINT    _IOR( 'u', 1, int)
+#define USERDMA_IOCTL_ALLOC    _IOWR('u', 2, struct userdma_buf_creation_data)
+#define USERDMA_IOCTL_GET_PHYS _IOWR('u', 3, struct userdma_phys_addr_data)
 
 #endif
--- linux/driver/userdma_main.c
+++ linux/driver/userdma_main.c
@@ -1,3 +1,5 @@
+#define pr_fmt(fmt) "%s:%s: " fmt, KBUILD_MODNAME, __func__
+
 #include <linux/device.h>
 #include <linux/dma-buf.h>
 #include <linux/dma-map-ops.h>
@@ -8,10 +10,11 @@
 #include "cont_dma_buf.h"
 #include "userdma.h"
 
-static void print_dmabuf_info(struct device *dev, struct sg_table* sg_tbl) {
+static long print_dmabuf_info(struct device *dev, struct sg_table* sg_tbl, void* unused) {
     struct scatterlist *sglist, *sg;
     unsigned int nents;
     int count, i;
+    (void)unused;
 
     sglist = sg_tbl->sgl;
     nents = sg_tbl->nents;
@@ -28,9 +31,36 @@ static void print_dmabuf_info(struct device *dev, struct sg_table* sg_tbl) {
     }
 
     dma_unmap_sg(dev, sglist, nents, DMA_TO_DEVICE);
+
+    return 0;
 }
 
-static long ioctl_print(struct device *dev, int buf_fd) {
+static long get_dmabuf_addr(struct device* dev, struct sg_table* sg_tbl, void* ctx) {
+    struct scatterlist *sglist, *sg;
+    unsigned int nents;
+    int count, i;
+    unsigned long* res;
+    res = ctx;
+
+    sglist = sg_tbl->sgl;
+    nents = sg_tbl->nents;
+
+    count = dma_map_sg(dev, sglist, nents, DMA_TO_DEVICE);
+
+    if(count != 1) {
+        return -EINVAL;
+    }
+
+    for_each_sg(sglist, sg, count, i) {
+        *res = sg_dma_address(sg);
+    }
+
+    dma_unmap_sg(dev, sglist, nents, DMA_TO_DEVICE);
+
+    return 0;
+}
+
+static long with_sg(struct device *dev, int buf_fd, long (*action)(struct device*, struct sg_table*, void*), void* ctx) {
     long ret;
     struct dma_buf *buf;
     struct dma_buf_attachment *at;
@@ -60,7 +90,7 @@ static long ioctl_print(struct device *dev, int buf_fd) {
         goto detach;
     }
 
-    print_dmabuf_info(dev, sg_tbl);
+    ret = action(dev, sg_tbl, ctx);
 
 //unmap:
     dma_buf_unmap_attachment(at, sg_tbl, DMA_TO_DEVICE);
@@ -72,9 +102,14 @@ exit:
     return ret;
 }
 
+static long ioctl_print(struct device *dev, int buf_fd) {
+    return with_sg(dev, buf_fd, print_dmabuf_info, 0);
+}
+
 static long ioctl_alloc_dmabuf(struct device* dev, unsigned long arg) {
     struct userdma_buf_creation_data d;
     long res;
+    pr_warn("Use /dev/dma_heap instead");
     if(copy_from_user(&d, (const void __user*)arg, sizeof(d))) {
         return -EFAULT;
     }
@@ -88,6 +123,23 @@ static long ioctl_alloc_dmabuf(struct device* dev, unsigned long arg) {
     return res;
 }
 
+static long ioctl_get_phys(struct device* dev, unsigned long arg) {
+    struct userdma_phys_addr_data d;
+    long res;
+    if(copy_from_user(&d, (const void __user*)arg, sizeof(d))) {
+        return -EFAULT;
+    }
+    res = with_sg(dev, d.fd, get_dmabuf_addr, &d.phys_addr);
+    if(res < 0) {
+        return res;
+    }
+    if(copy_to_user((void __user*)arg, &d, sizeof(d))) {
+        return -EFAULT;
+    }
+    return res;
+
+}
+
 static long handle_ioctl(struct file *filp, unsigned int ioctl,
               unsigned long arg) {
     struct device *dev;
@@ -98,6 +150,8 @@ static long handle_ioctl(struct file *filp, unsigned int ioctl,
             return ioctl_print(dev, (int)arg);
         case USERDMA_IOCTL_ALLOC:
             return ioctl_alloc_dmabuf(dev, arg);
+        case USERDMA_IOCTL_GET_PHYS:
+            return ioctl_get_phys(dev, arg);
         default:
             return -EINVAL;
     }
