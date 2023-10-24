f3a888b438a60ede122440e92c196d403632638a
natanbc <natanbc@users.noreply.github.com>
2023-10-24 05:38:38 -0300

    wb_cdc: register cdc output in target domain to help timing

--- gateware/zynq_gpu/wb_cdc.py
+++ gateware/zynq_gpu/wb_cdc.py
@@ -78,13 +78,13 @@ class WishboneCDC(Component):
         else:
             m.d.comb += self.i_bus.ack.eq(res_fifo.r_rdy)
 
-        m_request_in_flight = Signal()
+        i_request_in_flight = Signal()
         with m.If(self.i_bus.cyc & self.i_bus.stb):
-            m.d.comb += req_fifo.w_en.eq(~m_request_in_flight)
+            m.d.comb += req_fifo.w_en.eq(~i_request_in_flight)
             with m.If(req_fifo.w_en & req_fifo.w_rdy):
-                m.d.initiator += m_request_in_flight.eq(1)
+                m.d.initiator += i_request_in_flight.eq(1)
             with m.If(res_fifo.r_rdy):
-                m.d.initiator += m_request_in_flight.eq(0)
+                m.d.initiator += i_request_in_flight.eq(0)
 
         t_req = data.View(self._req, Signal(self._req))
         t_res = data.View(self._res, Signal(self._res))
@@ -94,7 +94,6 @@ class WishboneCDC(Component):
             self.t_bus.we.eq(t_req.we),
             self.t_bus.dat_w.eq(t_req.dat_w),
             self.t_bus.sel.eq(t_req.sel),
-            t_req.eq(req_fifo.r_data),
 
             t_res.dat_r.eq(self.t_bus.dat_r),
             res_fifo.w_data.eq(t_res),
@@ -103,11 +102,16 @@ class WishboneCDC(Component):
             m.d.comb += t_res.err.eq(self.t_bus.err)
 
         with m.If(req_fifo.r_rdy & res_fifo.w_rdy):
-            m.d.comb += [
+            m.d.target += [
+                t_req.eq(req_fifo.r_data),
                 self.t_bus.stb.eq(1),
                 self.t_bus.cyc.eq(1),
             ]
             with m.If(self.t_bus.ack | (self.t_bus.err if "err" in self._features else 0)):
+                m.d.target += [
+                    self.t_bus.stb.eq(0),
+                    self.t_bus.cyc.eq(0),
+                ]
                 m.d.comb += [
                     req_fifo.r_en.eq(1),
                     res_fifo.w_en.eq(1),
