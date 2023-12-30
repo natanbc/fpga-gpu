37d674a67294c633e3cc4447ccb3194e132b7c04
natanbc <natanbc@users.noreply.github.com>
2023-12-30 18:19:34 -0300

    hell: Implement clipping

--- linux/hell/gl/common.py
+++ linux/hell/gl/common.py
@@ -37,6 +37,14 @@ class FrontFace(enum.Enum):
     COUNTER_CLOCKWISE = enum.auto()
 
 
+CLIP_NEG_X = 0x01
+CLIP_POS_X = 0x02
+CLIP_NEG_Y = 0x04
+CLIP_POS_Y = 0x08
+CLIP_NEG_Z = 0x10
+CLIP_POS_Z = 0x20
+
+
 @dataclass(slots=True)
 class ClipVertex:
     x: float
@@ -46,6 +54,30 @@ class ClipVertex:
     r_s: float
     g_t: float
     b: float
+
+    def pos(self, idx: int) -> float:
+        match idx:
+            case 0: return self.x
+            case 1: return self.y
+            case 2: return self.z
+            case 3: return self.w
+            case _: raise IndexError()
+
+    def classify(self) -> int:
+        code = 0
+        if self.x < -self.w:
+            code |= CLIP_NEG_X
+        if self.x > self.w:
+            code |= CLIP_POS_X
+        if self.y < -self.w:
+            code |= CLIP_NEG_Y
+        if self.y > self.w:
+            code |= CLIP_POS_Y
+        if self.z < -self.w:
+            code |= CLIP_NEG_Z
+        if self.z > self.w:
+            code |= CLIP_POS_Z
+        return code
 
 
 @dataclass(slots=True)
@@ -89,7 +121,6 @@ def _to_clip(
         pvm,
         clip_attr_map: Callable[[_V], Tuple[float, float, float]],
 ) -> ClipVertex:
-    # pos = pvm @ np.array([v.x, v.y, v.z, 1.0])
     pos = pvm * glm.vec4(v.x, v.y, v.z, 1.0)
     r_s, g_t, b = clip_attr_map(v)
     return ClipVertex(
@@ -110,9 +141,64 @@ def _orientation(t: (ClipVertex, ClipVertex, ClipVertex)):
     return (bx - ax) * (cy - ay) - (by - ay) * (cx - ax)
 
 
+def _clip_against_plane(vertices: list[ClipVertex], plane: int) -> list[ClipVertex]:
+    sign, index = {
+        CLIP_NEG_X: (-1.0, 0),
+        CLIP_POS_X: (1.0, 0),
+        CLIP_NEG_Y: (-1.0, 1),
+        CLIP_POS_Y: (1.0, 1),
+        CLIP_NEG_Z: (-1.0, 2),
+        CLIP_POS_Z: (1.0, 2),
+    }[plane]
+
+    res = []
+    for i, vertex in enumerate(vertices):
+        v0 = vertices[-1] if i == 0 else vertices[i - 1]
+        v1 = vertex
+
+        p0 = v0.pos(index) * sign
+        w0 = v0.w
+        p1 = v1.pos(index) * sign
+        w1 = v1.w
+
+        if p0 < w0:
+            res.append(v0)
+
+        if (p0 < w0 and p1 >= w1) or (p0 >= w0 and p1 < w1):
+            denom = -p0 + p1 + w0 - w1
+            if abs(denom) > 0.001:
+                t = (-p0 + w0) / denom
+                res.append(ClipVertex(
+                    v0.x * (1.0 - t) + v1.x * t,
+                    v0.y * (1.0 - t) + v1.y * t,
+                    v0.z * (1.0 - t) + v1.z * t,
+                    v0.w * (1.0 - t) + v1.w * t,
+
+                    v0.r_s * (1.0 - t) + v1.r_s * t,
+                    v0.g_t * (1.0 - t) + v1.g_t * t,
+                    v0.b * (1.0 - t) + v1.b * t,
+                ))
+
+    return res
+
+
 def _clip(t: (ClipVertex, ClipVertex, ClipVertex)) -> Iterable[Tuple[ClipVertex, ClipVertex, ClipVertex]]:
-    # TODO
-    yield t
+    c0 = t[0].classify()
+    c1 = t[1].classify()
+    c2 = t[2].classify()
+    if (c0 & c1 & c2) != 0:
+        return
+
+    vertices = list(t)
+    if (c0 | c1 | c2) != 0:
+        for plane in [CLIP_NEG_X, CLIP_POS_X, CLIP_NEG_Y, CLIP_POS_Y, CLIP_NEG_Z, CLIP_POS_Z]:
+            vertices = _clip_against_plane(vertices, plane)
+
+        if len(vertices) > 0 and ((vertices[0].classify() & vertices[1].classify() & vertices[2].classify()) != 0):
+            return
+
+    for i in range(2, len(vertices) + 1):
+        yield vertices[0], vertices[i - 2], vertices[i - 1]
 
 
 def _to_screen(
@@ -120,15 +206,8 @@ def _to_screen(
         scale_device,
         screen_attr_map: Callable[[float, float, float], Tuple[int, int, int]],
 ) -> ScreenVertex:
-    assert -1.0 <= v.x <= 1.0
-    assert -1.0 <= v.y <= 1.0
-    assert -1.0 <= v.z <= 1.0
-    assert 0.0 <= v.r_s <= 1.0
-    assert 0.0 <= v.g_t <= 1.0
-    assert 0.0 <= v.b <= 1.0
-
     r_s, g_t, b = screen_attr_map(v.r_s, v.g_t, v.b)
-    v = ScreenVertex(
+    rv = ScreenVertex(
         int(((v.x / v.w) * 0.5 + 0.5) * scale_device[0]),
         int(((v.y / v.w) * -0.5 + 0.5) * scale_device[1]),
         int(((v.z / v.w) * -0.5 + 0.5) * scale_device[2]),
@@ -137,7 +216,14 @@ def _to_screen(
         b,
     )
 
-    return v
+    # assert 0 <= rv.x <= scale_device[0], f"{v.x/v.w}, {v}"
+    # assert 0 <= rv.y <= scale_device[1], f"{v.y/v.w}, {v}"
+    # assert 0 <= rv.z <= scale_device[2], f"{v.z/v.w}, {v}"
+    # assert 0 <= rv.r_s <= 255, f"{rv}"
+    # assert 0 <= rv.g_t <= 255, f"{v}"
+    # assert 0 <= rv.b <= 255, f"{v}"
+
+    return rv
 
 
 class GlCommon:
